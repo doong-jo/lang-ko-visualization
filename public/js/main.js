7f857de093dd51350bdf5a2d84a34365e0320fc8
doong-jo <sdong001@gmail.com>
2019-11-01 11:55:18 +0900

    feat: write js
    click requset button -> call fetchAPI, show loading spinner

---
+++ public/js/main.js
@@ -0,0 +1,44 @@
+function resize(obj) {
+    obj.style.height = '1px';
+    obj.style.height = `${12 + obj.scrollHeight}px`;
+}
+
+const requestText = document.querySelector('.sentence-input');
+const requestButton = document.querySelector('#btn-request');
+const resultArea = document.querySelector('.result-area');
+const resultLoading = document.querySelector('.result-loading');
+
+function toggleLoading(toggle) {
+    const { classList } = resultLoading;
+    if (toggle) {
+        classList.remove('hidden');
+        classList.add('show');
+        return;
+    }
+
+    classList.remove('show');
+    classList.add('hidden');
+}
+async function requestStartListener(event) {
+    const text = requestText.value;
+    console.log(text);
+    const response = await fetch(`http://localhost/gcp-lang?text=${text}`);
+
+    toggleLoading(false);
+
+    if (!response.ok) {
+        resultArea.innerHTML = 'request fail';
+        return;
+    }
+    const json = await response.json();
+
+    const stringResult = JSON.stringify(json, null, 4);
+    console.log(json);
+    resultArea.innerHTML = stringResult;
+}
+
+requestButton.addEventListener('click', (e) => {
+    resultArea.innerHTML = '';
+    toggleLoading(true);
+    requestStartListener(e);
+});
